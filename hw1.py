d46679d803ec9e67a984652aca601aeb1968fbd1
adva12 <85510819+adva12@users.noreply.github.com>
2021-07-10 14:54:26 +0300

    add hw1

--- hw1.py
+++ hw1.py
@@ -3,6 +3,6 @@ from time import sleep
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
  
-print("hi Adva")
+print("hi orr")
 driver1 = webdriver.Chrome(executable_path='C:/Users/USER/chromedriver_win32/chromedriver.exe')
-driver1.get("https://www.walla.co.il")
+driver1.get("https://www.ynet.co.il")
